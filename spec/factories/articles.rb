72d92f6017b3cd398c674319b924c03d036739e7
Sebastian Wilgosz <sebastian@driggl.com>
2020-11-15 01:30:15 +0100

    Add sample test for factory_bot introduction

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    title { "Sample article" }
+    content { "Sample content" }
+    slug { "sample-article" }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'tests a number to be positive' do
-    expect(1).to be_positive # be_positive => 1.positive? == true
-    expect(5).to be > 3
+  it 'tests article object' do
+    # article = Article.create({title: 'Sample article', content: 'Sample content' })
+    article = create(:article)
+    expect(article.title).to eq('Sample article')
   end
 end
