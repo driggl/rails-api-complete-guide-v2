8a025fb892b0979fa9e1ece0d5d8b66f3adfe49b
Sebastian Wilgosz <sebastian@driggl.com>
2020-11-15 01:31:19 +0100

    Refactor to meet rubocop expectations

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :article do
-    title { "Sample article" }
-    content { "Sample content" }
-    slug { "sample-article" }
+    title { 'Sample article' }
+    content { 'Sample content' }
+    slug { 'sample-article' }
   end
 end
