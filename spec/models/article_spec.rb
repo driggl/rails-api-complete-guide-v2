61be2a41cb05bbb2d54d706a095192da7cb24e4b
Sebastian Wilgosz <sebastian@driggl.com>
2020-11-15 02:24:21 +0100

    Add basic article validations tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,9 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'tests article object' do
-    # article = Article.create({title: 'Sample article', content: 'Sample content' })
-    article = create(:article)
-    expect(article.title).to eq('Sample article')
+  describe '#validations' do
+    let(:article) { build(:article) }
+
+    it 'tests that factory is valid' do
+      expect(article).to be_valid # article.valid? == true
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
   end
 end
