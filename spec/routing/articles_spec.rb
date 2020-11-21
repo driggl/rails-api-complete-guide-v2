2c72abbd0a323aa31931b5e360697d14ef2b642c
Sebastian Wilgosz <sebastian@driggl.com>
2020-11-21 15:06:15 +0100

    Add routing tests for listing articles

---
+++ spec/routing/articles_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe '/articles routes' do
+  it 'routes to articles#index' do
+    # expect(get '/articles').to route_to(controller: 'articles', action: 'index')
+    aggregate_failures do
+      expect(get('/articles')).to route_to('articles#index')
+      expect(get('/articles?page[number]=3')).to(
+        route_to('articles#index', page: { 'number' => '3' })
+      )
+    end
+  end
+end
