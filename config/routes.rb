3638dc0d3ef91c22fcb846a45b151b8d37dcc745
Sebastian Wilgosz <sebastian@driggl.com>
2020-11-21 15:06:36 +0100

    Add routing for listing articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/articles', to: 'articles#index'
+  resources :articles, only: [:index]
 end
