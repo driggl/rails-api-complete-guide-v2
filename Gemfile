e6ae7c7c65b8c72e2d96b2379ebaa3853d457183
Sebastian Wilgosz <sebastian@driggl.com>
2021-02-14 01:55:42 +0100

    Add error handling for all controller actions

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'puma', '~> 4.1'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'jsom-pagination'
 gem 'jsonapi-serializer'
+gem 'jsonapi_errors_handler'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include JsonapiErrorsHandler
+
+  ErrorMapper.map_errors!(
+    'ActiveRecord::RecordNotFound' =>
+      'JsonapiErrorsHandler::Errors::NotFound'
+  )
+  rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
 end
