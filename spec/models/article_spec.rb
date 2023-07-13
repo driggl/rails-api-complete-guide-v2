efb800267c913ebcf1cb55b3021485ab99691643
Sebastian Wilgosz <sebastian@driggl.com>
2023-07-13 04:39:40 +0200

    Add dummy test example

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'tests a number to be positive' do
+    expect(1).to be_positive # be_positive => 1.positive? == true
+    expect(5).to be > 3
+  end
 end
